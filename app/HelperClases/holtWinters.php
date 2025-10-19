30c7e01f4c21337e40cc0f6fb3f60ba1b062f452
Angel Ricardo <angelrcdma@icloud.com>
2025-10-19 03:32:56 -0400

    fix initialization

--- app/HelperClases/holtWinters.php
+++ app/HelperClases/holtWinters.php
@@ -6,18 +6,38 @@ use Illuminate\Support\Facades\DB;
 
 class holtWinters
 
-{
-    //Necesitamos los datos de las ventas menusales
 
+
+{
+
+
+    //Propiedades
     private $userID;
     private $productID;
 
+
+    //Inicializaciones
     private $years = [];
     private $seasonal_factor = [];
 
     private $nivel_inicial = [];
 
     private $tendencia_inicial = [];
+
+
+
+
+
+
+
+    /*
+     *      Se encarga de realizar las inicializaciones para los datos proporcionados de
+     *   año, producto y usuario. Esta será una clase estática para calcular.
+     *
+     *
+     *
+     *
+     * */
 
     public function __construct($initialYear , $userID , $productID){
 
@@ -26,38 +46,49 @@ class holtWinters
 
 
         $years = now()->year - $initialYear;
-        $sum = $years;
 
-        for ($i = 0; $i < $years ; $i++) {
+        $sum = 0;
 
+
+        for ($i = 0; $i <= $years ; $i++) {
+
+            $this->years[] = $initialYear + $sum;
             $sum ++;
-            $this->years[$i] = $sum;
 
         }
 
+
         $this->nivel_inicial = $this->get_nivelInicial($userID , $productID);
         $this->tendencia_inicial = $this->get_tendenciaInicial($userID , $productID);
+        $this->seasonal_factor = $this->get_factoresE($initialYear + 1);
 
 
         //   $this->ventas_iniciales = DB::select("SELECT * FROM holt_level($userID, 20, 2023)");
     }
 
 
-    //Retorna el promedio inicial para el primer anho
+
+
+
+
+    //Inicializaciones, métodos
     public function get_nivelInicial($userID, $productID){
 
-        $results = DB::select("SELECT * FROM holt_level($userID, $productID, $this->years[0])");
+        $year = $this->years[0];
+
+
+        $results = DB::select("SELECT total_vendido FROM holt_level($userID, $productID,$year )");
+
 
         $sum= 0;
+
         foreach ( $results as $month) {
+
             $sum += $month->total_vendido;
         }
-         return $sum/12;
-    }
 
-    //Retorna el conjunto de datos para cada producto en un anho, las ventas mensuales
-    static function getData($user, $product, $year = 2023){
-        return DB::select("SELECT * FROM holt_level($user, $product, $year)");
+        return  $sum/12;
+
     }
 
     public function get_tendenciaInicial($userID, $productID){
@@ -81,6 +112,16 @@ class holtWinters
 
 
 
+    //Retorna el conjunto de datos para cada producto en un anho, las ventas mensuales
+    static function getData($user, $product, $year = 2023){
+        return DB::select("SELECT * FROM holt_level($user, $product, $year)");
+    }
+
+
+
+
+
+    //Métodos auxiliares para calcular los valores
     public function calcular_nivel($a = 0.3, $venta, $nivel_anterior, $tendencia_anterior, $temp_mes){
 
         return round(($a * ($venta / $temp_mes) + (1 - $a) * ($nivel_anterior + $tendencia_anterior)),2);
@@ -102,17 +143,15 @@ class holtWinters
 
     public function get_factoresE( $year = 2023){
 
-        $interval = now()->year - $initialYear;
 
-        $data = self::getData($initialYear , $userID , $productID);
+        $data = self::getData($this->userID , $this->productID, $year);
 
         $factores = array();
         $j = 0;
 
         for ($i = 1; $i < 12; $i++) {
 
-        //Calcular el factor en i y agregarlo a un array
-        $factores[$j] = $data[$j]->total_vendido/( $this->nivel_inicial + ($i - 1)*$this->tendencia_inicial  );
+        $factores[$j] = $data[$j]->total_vendido/( $this->nivel_inicial + ( 1 - $i)*$this->tendencia_inicial);
         $j++;
 
         }
--- app/Http/Controllers/suggestions_controller.php
+++ app/Http/Controllers/suggestions_controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\HelperClases\holtWinters;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -21,14 +22,17 @@ class suggestions_controller extends Controller
     {
 
         $userID = $request->user()->id;
+        $productoID = 20;
 
         //Calcularemos la tendencia para un mes, un producto, a partir de datos de tres años anteriores
 
         //Obtener los resultados del primer año para calcular el nivel inicial, luego a partir de ahi se calculan recursivamente
         //Los valores del nivel para cada mes de ventas, teniendo c
 
-        $res = DB::select("SELECT * FROM holt_level($userID, 20, 2023)");
+        $holt = new holtWinters(2023, $userID, $productoID);
 
+        dd($holt, $holt::getData($userID, $productoID));
+//        $res = $holt->get_nivelInicial($userID, $productoID);
 
         $string = "";
 
